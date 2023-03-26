b07fa33c192a482af71e73396fc1c5eb0a0f4d95
viniciusanchieta <vinicius.anchieta@outlook.com>
2023-03-26 20:00:35 -0300

    chore: separate setup and config of jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   preset: 'react-native',
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',
-    '@testing-library/react-native/cleanup-after-each'
-  ]
+  setupFilesAfterEnv: ['./jest.setup.js']
 };
