228f5c7b2ca4f3c458160da746a45eeaf6ff1b10
viniciusanchieta <vinicius.anchieta@outlook.com>
2023-03-31 12:44:24 -0300

    feat: create base theme

---
+++ src/presentation/common/themes/base-theme.ts
@@ -0,0 +1,20 @@
+const baseTheme = {
+  colors: {
+    bgMain: '#FFFFFF',
+    bgSecondary: '#F4F5F9',
+    black: '#000000',
+    green: '#6CC51D',
+    greenLight: '#D9F2D9',
+    red: '#EF574B',
+    gray: '#868889',
+    grayLight: '#EBEBEB',
+    orangeLight: '#FDEFD5',
+    orangeDark: '#E8AD41'
+  },
+  fonts: {
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold'
+  }
+};
+
+export default baseTheme;
